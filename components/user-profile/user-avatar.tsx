ee73dbb6428fd38088947580489324f84834e5a0
RaviSingnath <ravisingnath@gmail.com>
2026-06-03 20:36:38 +0530

    Add avatar icon background change

--- components/user-profile/user-avatar.tsx
+++ components/user-profile/user-avatar.tsx
@@ -13,7 +13,7 @@ export default function UserAvatar() {
 
   return (
     <>
-      <div className="relative w-20 h-20 rounded-fullbg-white">
+      <div className="relative w-20 h-20 rounded-full bg-white">
         {user?.avatar_url ? (
           <Image
             width={80}
@@ -30,7 +30,7 @@ export default function UserAvatar() {
         <button onClick={openModal}>
           <ImagePlus
             size={24}
-            className="absolute p-1 top-0 right-0 rounded-full border-[1.5px] stroke-gray-500 hover:stroke-dark-900 border-gray-200 hover:bg-gray-100 hover:stroke-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:stroke-gray-400 dark:hover:bg-gray-800 dark:hover:stroke-white"
+            className="absolute p-1 top-0 right-0 rounded-full border-[1.5px] stroke-gray-500 hover:stroke-dark-900 border-gray-200 bg-brand-50 hover:bg-brand-100 hover:stroke-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:stroke-gray-400 dark:hover:bg-gray-800 dark:hover:stroke-white"
           />
         </button>
       </div>
